433c0afac713d4ae003dd204a45f3704680a3dfa
NicolasRichel <nicolasrichel.github@gmail.com>
2018-02-16 23:20:23 +0100

    add basic html to task-form

--- src/components/task-form.js
+++ src/components/task-form.js
@@ -1,3 +1,23 @@
 export default {
-  template: `<div>Task Form</div>`
+  template: `
+  <div>
+    <label>Name :</label>
+    <input type="text"/>
+    <label>Priority :</label>
+    <select>
+      <option>0</option>
+      <option>1</option>
+      <option>2</option>
+    </select>
+    <label>Status :</label>
+    <select>
+      <option>0/option>
+      <option>1</option>
+      <option>2</option>
+    </select>
+    <label>Description :</label>
+    <textarea></textarea>
+    <button></button>
+  </div>
+  `
 };
